738c52162cd49c6b4bd0af3bb892a5ba4d1f8691
freddinalcantara <freddyn.alcantara@gmail.com>
2025-04-20 21:59:46 -0400

    Ordernar listado

--- src/components/Sidebar.jsx
+++ src/components/Sidebar.jsx
@@ -5,12 +5,12 @@ const Sidebar = ({ onSelectGenre, isOpen }) => {
     <div className={`sidebar ${isOpen ? "open" : ""}`}>
       <h2>Géneros</h2>
       <ul>
+        <li onClick={() => onSelectGenre(null)}>Todos</li>
         {genres.map((genre) => (
           <li key={genre} onClick={() => onSelectGenre(genre)}>
             {genre}
           </li>
         ))}
-        <li onClick={() => onSelectGenre(null)}>Todos</li>
       </ul>
     </div>
   );
